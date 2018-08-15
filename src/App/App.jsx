6857b440d9007108a8cf7ee48201f1003de24992
anashkin <strawdragon@yandex.ru>
2018-08-15 17:32:08 +0300

    simple banner site begin

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import Header from './Header/Header';
+/* import Header from './Header/Header';
 import Content from './Content/Content';
-import Footer from './Footer/Footer';
+import Footer from './Footer/Footer'; */
+import Banner from './Banner/Banner';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        {/* <Header />
         <Content />
-        <Footer />
+        <Footer /> */}
+        <Banner />
       </div>
     );
   }
---
+++ src/App/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Banner.css';
+
+class Banner extends Component {
+  render() {
+    return (
+      <div className="App-banner">
+        <h1>Привет, Мир!</h1>
+      </div>
+    );
+  }
+}
+
+export default Banner;
